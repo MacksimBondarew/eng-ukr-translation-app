5fb850d4c4c8e7b9ffd15dab5076efa44b6b2b65
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2023-06-10 17:40:14 +0300

    upd

--- src/components/sharedLayout/SharedLayout.jsx
+++ src/components/sharedLayout/SharedLayout.jsx
@@ -165,8 +165,8 @@ export const SharedLayout = () => {
                                     sx={{ p: 0 }}
                                 >
                                     <Avatar
-                                        alt="Remy Sharp"
-                                        src="/static/images/avatar/2.jpg"
+                                        alt="I"
+                                        src="../../../public/logo192.png"
                                     />
                                 </IconButton>
                             </Tooltip>
